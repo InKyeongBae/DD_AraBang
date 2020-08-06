d519cbacbc69c15db66c5a0b95ae1465dfbea3cd
jiyoung98 <jiyoung15105@naver,com>
2020-08-06 22:04:41 +0900

    0806jiyoung_review

--- DDAraBang/review/admin.py
+++ DDAraBang/review/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Place)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
--- DDAraBang/review/models.py
+++ DDAraBang/review/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.shortcuts import render, redirect, reverse
 
-# Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=25)
+    lat = models.CharField(max_length=25)
+    lng = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}'.format(self.name)
