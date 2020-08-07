529625739afa1e15cd6d438d4529cc030f493fdc
lee <secret>
2020-08-08 01:39:11 +0900

    8/7 작업

--- DDAraBang/community/urls.py
+++ DDAraBang/community/urls.py
@@ -5,7 +5,7 @@ app_name = 'community'
 
 urlpatterns = [
     # path('', views.main_html),
-    path('<int:pk_1>/', views.community_list),
+    path('<int:pk_1>/', views.community_list, name='community_list'),
     path('<int:pk_1>/<int:pk>/', views.post_list, name='post_list'),
     path('<int:pk_1>/<int:pk>/<int:pk_3>/', views.post_detail),
     path('<int:pk_1>/<int:pk>/write/', views.post_write, name='write'),
