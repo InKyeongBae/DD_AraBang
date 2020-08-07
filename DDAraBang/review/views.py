eb1e82050bf526cdcaf254c4773f50f06321199a
kains123 <kains0417@naver.com>
2020-08-07 14:28:38 +0900

    두 번째 수정 community

--- DDAraBang/review/views.py
+++ DDAraBang/review/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+import json
+import requests
+import geocoder
+import openpyxl
+import time
+from .models import *
 
 # Create your views here.
+
+
+def showhouses(request):
+    places = Place.objects.all()
+
+    context = {
+        'places': places,
+    }
+
+    return render(request, 'review/showhouses.html', context=context)
+
+
+def createaddress(request):
+    # Get
+    if request.method == 'GET':
+        return render(request, 'review/createaddress.html', context={})
+    # Post
+    elif request.method == 'POST':
+        address = request.POST['address']
+        context = {
+            'address': address
+        }
+        return render(request, 'review/checkaddress.html', context=context)
+
+
+def checkaddress(request):
+    # Post
+    if request.method == 'POST':
+        address = request.POST['address']
+        lat = request.POST['lat']
+        lng = request.POST['lng']
+        Place.objects.create(name=address, lat=lat, lng=lng)
+
+        url = reverse('review:createaddress')
+        return redirect(to=url)
