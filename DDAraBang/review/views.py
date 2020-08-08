bae556cd06120051c11d79547e021d010f451c54
InKyeong <hwang0809@cau.ac.kr>
2020-08-08 17:56:31 +0900

    foreign key 연결 admin에서 봤을 때 성공

--- DDAraBang/review/views.py
+++ DDAraBang/review/views.py
@@ -59,29 +59,42 @@ def createaddress(request):
     money = request.POST['money']
     recommend = request.POST['recommend']
 
+    place, is_created = Place.objects.get_or_create(
+        name=houseaddress,
+        lat=lat,
+        lng=lng
+    )
 
-    try:
-        houses = Place.objects.get(name=houseaddress)
-    except :
-        houses = None
-
-    if houses:
-        print("있는 집")
-
-        ReviewForm.objects.create(houseaddress=houseaddress, lat=lat, lng=lng, image=image, floor=floor,
-                                  advantage=advantage, disadvantage=disadvantage, water=water,
-                                  waterplus=waterplus, light=light, lightplus=lightplus, noise=noise,
-                                  noiseplus=noiseplus,
-                                  security=security, securityplus=securityplus, bug=bug, bugplus=bugplus, money=money,
-                                  recommend=recommend)
-    else:
-        print('없는 새로운 집')
-        Place.objects.create(name=houseaddress, lat=lat, lng=lng)
-        ReviewForm.objects.create(houseaddress=houseaddress, lat=lat, lng=lng, image=image, floor=floor, advantage=advantage, disadvantage=disadvantage, water=water,
+    # TODO : place 쓰기
+    ReviewForm.objects.create(place=place, image=image, floor=floor,
+                              advantage=advantage, disadvantage=disadvantage, water=water,
                               waterplus=waterplus, light=light, lightplus=lightplus, noise=noise,
                               noiseplus=noiseplus,
                               security=security, securityplus=securityplus, bug=bug, bugplus=bugplus, money=money,
                               recommend=recommend)
+
+    # try:
+    #     houses = Place.objects.get(name=houseaddress)
+    # except :
+    #     houses = None
+
+    # if houses:
+    #     print("있는 집")
+    #
+    #     ReviewForm.objects.create(houseaddress=houseaddress, lat=lat, lng=lng, image=image, floor=floor,
+    #                               advantage=advantage, disadvantage=disadvantage, water=water,
+    #                               waterplus=waterplus, light=light, lightplus=lightplus, noise=noise,
+    #                               noiseplus=noiseplus,
+    #                               security=security, securityplus=securityplus, bug=bug, bugplus=bugplus, money=money,
+    #                               recommend=recommend)
+    # else:
+    #     print('없는 새로운 집')
+    #     Place.objects.create(name=houseaddress, lat=lat, lng=lng)
+    #     ReviewForm.objects.create(houseaddress=houseaddress, lat=lat, lng=lng, image=image, floor=floor, advantage=advantage, disadvantage=disadvantage, water=water,
+    #                           waterplus=waterplus, light=light, lightplus=lightplus, noise=noise,
+    #                           noiseplus=noiseplus,
+    #                           security=security, securityplus=securityplus, bug=bug, bugplus=bugplus, money=money,
+    #                           recommend=recommend)
 
     # context = {
     #     'address': address,
