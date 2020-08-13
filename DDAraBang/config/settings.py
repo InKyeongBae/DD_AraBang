daff113d40e25b4fa9f9fa138be0573d8ce4cc10
jiyoung98 <jiyoung15105@naver,com>
2020-08-13 17:38:01 +0900

    merge전 커밋

--- DDAraBang/config/settings.py
+++ DDAraBang/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '2yv1@3uovww1j9f^!7p6h^tn0b6q=9t&u95wawm!ypaf11b9%p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,5 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # 미디어 
 AUTH_USER_MODEL = "user.User"
